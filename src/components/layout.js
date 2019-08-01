2f2aadff398af36455aaca177e69494a126109ad
Jovaan <bienvenujovan@gmail.com>
2019-08-01 15:38:41 +0200

    Change url snipcart

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,16 @@ const Layout = ({ children }) => (
   <>
     <Helmet>
       <html lang="fr" />
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+      <script
+        src="https://cdn.snipcart.com/scripts/2.0/snipcart.js"
+        id="snipcart"
+      ></script>
+      <link
+        href="https://cdn.snipcart.com/themes/2.0/base/snipcart.min.css"
+        rel="stylesheet"
+        type="text/css"
+      />
     </Helmet>
     <Navbar />
     {children}
